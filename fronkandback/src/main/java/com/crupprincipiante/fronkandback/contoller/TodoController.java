038581dd18d42641df6a229aa49c3f6f63f7e5bb
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-09-19 22:13:52 -0500

    Validando conexion base de datos lista

--- fronkandback/src/main/java/com/crupprincipiante/fronkandback/contoller/TodoController.java
+++ fronkandback/src/main/java/com/crupprincipiante/fronkandback/contoller/TodoController.java
@@ -1,5 +1,44 @@
 package com.crupprincipiante.fronkandback.contoller;
 
+import com.crupprincipiante.fronkandback.entity.Todo;
+import com.crupprincipiante.fronkandback.service.TodoServiceI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
 
+@Service
+@CrossOrigin(origins = "http://localhost:3306")
 public class TodoController {
+
+    @Autowired
+    private TodoServiceI service;
+
+    @GetMapping(value = "api/todos")
+    public Iterable<Todo> list(){
+        return service.list();
+    }
+
+    @PostMapping(value = "api/todo")
+    public Todo save(@RequestBody Todo todo){
+        return service.save(todo);
+    }
+
+    @PutMapping(value = "api/todo")
+    public Todo update(@RequestBody Todo todo){
+        if(todo.getId() != null){
+            return service.save(todo);
+        }
+        throw new RuntimeException("No existe el id para actualziar");
+    }
+
+    @DeleteMapping(value = "api/{id}/todo")
+    public void delete(@PathVariable("id")int id){
+        service.delete(id);
+    }
+
+    @GetMapping(value = "api/{id}/todo")
+    public Todo get(@PathVariable("id") int id){
+        return service.get(id);
+    }
+
 }
